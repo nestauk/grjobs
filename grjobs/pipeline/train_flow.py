6b2d52abdbdebc7777e1714c0ae46d33bfbe00b4
India Kerle <india.kerle@nesta.org.uk>
2021-08-03 09:47:09 +0200

    updated train_flow

--- grjobs/pipeline/train_flow.py
+++ grjobs/pipeline/train_flow.py
@@ -20,7 +20,6 @@ class TrainGreenFlow(FlowSpec):
     
     @step
     def start(self):
-        self.green_words = get_expanded_green_words()
         self.labelled_data = load_json_from_s3('final_training_set')
         self.model = GreenClassifier() 
         self.next(self.split_data)
@@ -29,22 +28,26 @@ class TrainGreenFlow(FlowSpec):
     def split_data(self):
         self.X_train, self.X_test, self.y_train, self.y_test = self.model.split_data(
             self.labelled_data, 0.1, verbose=True)
+        print('split training data!')
         self.next(self.fit_model)
 
     @step
     def fit_model(self):
         self.model.fit(self.X_train, self.y_train)
         self.predictions = self.model.predict(self.X_test)
+        print('made predictions!')
         self.next(self.evaluate)
 
     @step
     def evaluate(self):
         green_class_results = self.model.evaluate(self.y_test, self.predictions, verbose = True)
+        print('evaluated model!')
         self.next(self.save)
 
     @step
     def save(self):
         self.model.save_model('best_model')
+        print('saved model!')
         self.next(self.end)
 
     @step
